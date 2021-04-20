d59082343e8ac73689afb08dac0627e68c73278d
Brantley Harris <deadwisdom@gmail.com>
2021-04-20 15:44:25 -0600

    MOre stuff

--- src/statecontroller.ts
+++ src/statecontroller.ts
@@ -1,7 +1,13 @@
 import { Provider, Storable } from "./provider";
 
 interface ControllerHost {
-  addController(controller:Controller): void;
+  addController(controller: BaseController): void;
+}
+
+interface SyncPropOptions {
+  provider: Provider;
+  keyProp: string;
+  syncProp: string;
 }
 
 interface StateDoc {
@@ -9,23 +15,23 @@ interface StateDoc {
   key: any;
   cache: Map<string, Storable>;
 
-  constructor(provider:Provider, key:any): StateDoc;
+  constructor(provider: Provider, key: any): StateDoc;
 
   get(key: string): Storable;
   set(key: string, val: Storable): void;
-  update(props:Object): void;
-  update(props:Map<string, Storable>): void;
+  update(props: Object): void;
+  update(props: Map<string, Storable>): void;
   has(key: string): boolean;
-  save(options:any): Promise<StateDoc>;
-  load(options:any): Promise<StateDoc>;
-  delete(options:any): Promise<StateDoc>;
-  subscribe(callback:any): Function;
+  save(options: any): Promise<StateDoc>;
+  load(options: any): Promise<StateDoc>;
+  delete(options: any): Promise<StateDoc>;
+  subscribe(callback: any): Function;
 }
 
 export class BaseController {
   _host: ControllerHost;
 
-  constructor(host:ControllerHost) {
+  constructor(host: ControllerHost) {
     this._host = host;
     this._host.addController(this);
   }
@@ -36,17 +42,28 @@ export class BaseController {
   hostUpdated(changedProps: Map<string, any>): void {}
 }
 
-
 export class DocController extends BaseController {
   _provider: Provider;
+  _keyProp: string;
 
-  constructor(host:ControllerHost, provider:Provider) {
+  constructor(host: ControllerHost, provider: Provider, keyProp: string) {
     super(host);
     this._provider = provider;
+    this._keyProp = keyProp;
+  }
+
+  get key(): any {
+    return (this._host as any)[this._keyProp];
+  }
+
+  hostUpdate(changedProps: Map<string, any>): void {
+    if (changedProps.has(this._keyProp)) {
+      this.unsubscribe();
+      this.subscribe();
+    }
   }
 
   hostConnected() {
-
     // Start listening
   }
 
@@ -55,13 +72,11 @@ export class DocController extends BaseController {
   }
 
   get(key: string): Storable {
-    
+    return this._host;
   }
 }
 
-export class PropController extends DocController {
-  host: ControllerHost
-
+export class SyncPropController extends DocController {
   hostConnected(): void {
     // Start listening
   }
@@ -70,9 +85,7 @@ export class PropController extends DocController {
     // Stop listening
   }
 
-  hostUpdate(changedProps: Map<string, any>) {
-    
-  }
+  hostUpdate(changedProps: Map<string, any>) {}
 
   hostUpdated(changedProps: Map<string, any>) {
     // Save...
